7fee06840189b11b9d6f7255aeb955de3e0445c9
Shrijana Ghimire <sghimire32@gatech.edu>
2020-06-16 13:50:10 -0700

    test site initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `Zoom Starter Personal Blog`,
+    author: `Shrijana`,
+    description: `Welcome to the Zoom Developer Platform.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `zoom_us`,
     },
   },
   plugins: [
